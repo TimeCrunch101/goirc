62aff1bf9dec0ecc19c22083ffed38d9334f9777
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2025-05-21 11:50:49 -0400

    feat: error handling for registration

--- internal/services/irc/registration.go
+++ internal/services/irc/registration.go
@@ -2,10 +2,21 @@ package irc
 
 import (
 	"fmt"
+	"log"
 )
 
 func SendRegistrationMsg(u *User) {
 	reply := fmt.Sprintf(":localhost 001 %s :Welcome to our IRC server!, %s!%s@cincitechlabs.com\r\n", u.Nick, u.Nick, u.User)
-	u.UserWriter.WriteString(reply)
-	u.UserWriter.Flush()
+
+	if _, err := u.UserWriter.WriteString(reply); err != nil {
+		log.Printf("ERROR IN REGISTRATION WRITESTRING: %v", err)
+		HandleDisconnect(u)
+		return
+	}
+
+	if err := u.UserWriter.Flush(); err != nil {
+		log.Printf("ERROR IN REGISTRATION FLUSH: %v", err)
+		HandleDisconnect(u)
+		return
+	}
 }
