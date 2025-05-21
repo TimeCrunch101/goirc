2b42f504cd48ffd22f8aabc7bc1b54fff7d4c9a0
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2025-05-21 11:50:06 -0400

    feat: testing for user fetching

--- internal/services/httpserver/gets.go
+++ internal/services/httpserver/gets.go
@@ -1,7 +1,10 @@
 package httpserver
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/timecrunch101/goirc/internal/services/irc"
 )
 
 func NewLogger(handlerToWrap http.Handler) *Logger {
@@ -21,6 +24,15 @@ func NotFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetIrcUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("HERE")
+	for k, v := range irc.Channels {
+		fmt.Printf("CHANNEL NAME: %s\r\n", k)
+
+		for k, v := range v.Users {
+			fmt.Printf("FOUND USER: %s\nREGISTERED?: %v\nCONNECTED TO CHANNEL?: %v\n", k.Nick, k.Registered, v)
+		}
+
+	}
 
 	SendMessage(w, "SUCCESS", "No users found", nil, http.StatusOK)
 }
